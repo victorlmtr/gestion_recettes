125052eef335f95289cfc75dbb5a90c636208368
victorlmtr <victor.lemaitre1@gmail.com>
2024-06-10 14:55:14 +0200

    NavigationBar and navigation now working :)

---
+++ app/src/main/java/com/groupe/gestionrecettes/ui/screens/RecipesScreen.kt
@@ -0,0 +1,59 @@
+package com.groupe.gestionrecettes.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.groupe.gestionrecettes.R
+import com.groupe.gestionrecettes.ui.theme.GestionRecettesTheme
+
+@Composable
+fun RecipesScreen(navController: NavController) {
+    RecipesScreenContent()
+}
+
+@Composable
+fun RecipesScreenContent() {
+    GestionRecettesTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(15.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Recipes Screen",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(vertical = 20.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RecipesScreenPreview() {
+    RecipesScreenContent()
+}
