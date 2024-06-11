db56914d4334da439a53da1d47ab4d563ac45bf4
victorlmtr <victor.lemaitre1@gmail.com>
2024-06-11 13:22:44 +0200

    NavigationBar and navigation now working - isolated and cleaned the NavBar and navigation logic

--- app/src/main/java/com/groupe/gestionrecettes/ui/composables/MainNavigationContent.kt
+++ app/src/main/java/com/groupe/gestionrecettes/ui/composables/MainNavigationContent.kt
@@ -9,10 +9,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -28,23 +24,25 @@ import com.groupe.gestionrecettes.ui.screens.ProfileScreen
 import com.groupe.gestionrecettes.ui.screens.RecipesScreen
 
 @Composable
-fun BottomNavigationBar(navController: NavHostController, currentDestination: String?) {
-    NavigationBar {
-        BottomNavigationItem().bottomNavigationItems().forEachIndexed { _, navigationItem ->
-            NavigationBarItem(
-                selected = navigationItem.route == currentDestination,
-                label = { Text(navigationItem.label) },
-                icon = { Icon(navigationItem.icon, contentDescription = navigationItem.label) },
-                onClick = {
-                    navController.navigate(navigationItem.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }
-            )
+fun MainNavigationContent(navController: NavHostController = rememberNavController()) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            BottomNavigationBar(navController = navController, currentDestination = currentDestination?.route)
+        }
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = Screens.Home.route,
+            modifier = Modifier.padding(paddingValues = paddingValues)
+        ) {
+            composable(Screens.Home.route) { HomeScreen(navController) }
+            composable(Screens.Recipes.route) { RecipesScreen(navController) }
+            composable(Screens.Pantry.route) { PantryScreen(navController) }
+            composable(Screens.Profile.route) { ProfileScreen(navController) }
         }
     }
 }
